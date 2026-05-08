924a298af80a14ca5a48f1ff40c5d4e195db844d
jecheon-coding <manyoung.ji@gmail.com>
2026-05-08 16:23:39 +0900

    feat: 세션 유효시간 2시간으로 설정

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -84,7 +84,7 @@ export const authOptions: NextAuthOptions = {
 
   secret:  process.env.NEXTAUTH_SECRET,
   pages:   { signIn: "/login" },
-  session: { strategy: "jwt" },
+  session: { strategy: "jwt", maxAge: 60 * 60 * 2 },
 
   callbacks: {
     async signIn({ user, account }) {
